32126bde650ed1fd403c67294b9f496556b7673e
Joe Fontana <jfonv@allstate.com>
2016-06-15 16:29:02 -0500

    first commit

---
+++ test/account.spec.js
@@ -0,0 +1,11 @@
+let expect = require('chai').expect;
+let account = require('../lib/account');
+
+describe('Account', function(){
+describe('Account', function(){
+  it('should do shit', function() {
+    const result = [1,2,3].mymap(n=>n*n);
+    expect(result).to.have.length(3);
+  });
+});
+});
